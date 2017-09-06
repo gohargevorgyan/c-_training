c1b94cc5cd7a784cc201940e2c7a897b8a585bfb
Gohar Gevorgyan <gohargevorgyan95@gmail.com>
2017-09-06 14:10:30 +0400

    Move to Method

--- GitFirstProject/GitFirstProject/Program.cs
+++ GitFirstProject/GitFirstProject/Program.cs
@@ -10,6 +10,8 @@ namespace GitFirstProject
     {
         static void Main(string[] args)
         {
+
+
             Console.WriteLine("Enter your array length!");
             int length = int.Parse(Console.ReadLine());
 
@@ -21,18 +23,7 @@ namespace GitFirstProject
             }
             ///////////////////////insertion sort
 
-            for (int i = 1; i < array.Length; i++)
-            {
-                int key = array[i];
-                int j = i - 1;
-                while (j >= 0 && array[j] > key)
-                {
-                    array[j + 1]= array[j] ;
-                    j--;
-                }
-                array[j + 1] = key;
-            }
-
+            InsertionSort(array);
             Console.WriteLine("Sorted Array");
             for (int i = 0; i < array.Length; i++)
             {
@@ -41,5 +32,21 @@ namespace GitFirstProject
 
             Console.ReadLine();
         }
+            
+        public static void InsertionSort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
     }
+
 }
